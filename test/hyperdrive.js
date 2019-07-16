dcd87862930a809b513af42150d581f0c11547dc
Andrew Osheroff <andrewosh@gmail.com>
2019-07-16 23:09:58 +0200

    Added readFile/writeFile chunks + unmount + tests for both

--- test/hyperdrive.js
+++ test/hyperdrive.js
@@ -24,6 +24,30 @@ test('can write/read a file from a remote hyperdrive', async t => {
   t.end()
 })
 
+test('can write/read a large file from a remote hyperdrive', async t => {
+  const { client, cleanup } = await createOne()
+
+  const content = Buffer.alloc(3.9e6 * 10.11).fill('abcdefghi')
+
+  try {
+    const { opts, id } = await client.drive.get()
+    t.true(opts.key)
+    t.same(id, 1)
+
+    await client.drive.writeFile(id, 'hello', content)
+
+    const contents = await client.drive.readFile(id, 'hello')
+    t.same(contents, content)
+
+    await client.drive.close(id)
+  } catch (err) {
+    t.fail(err)
+  }
+
+  await cleanup()
+  t.end()
+})
+
 test('can stat a file from a remote hyperdrive', async t => {
   const { client, cleanup } = await createOne()
 
@@ -142,4 +166,50 @@ test('can mount a drive within a remote hyperdrive', async t => {
 
   await cleanup()
   t.end()
+})
+
+test.only('can unmount a drive within a remote hyperdrive', async t => {
+  const { client, cleanup } = await createOne()
+
+  try {
+    const { opts: opts1, id: id1 } = await client.drive.get()
+    t.true(opts1.key)
+    t.same(id1, 1)
+
+    const { opts: opts2, id: id2 } = await client.drive.get()
+    t.true(opts2.key)
+    t.same(id2, 2)
+    t.notEqual(opts1.key, opts2.key)
+
+    const noVersion = { ...opts2, version: null }
+
+    await client.drive.mount(id1, 'a', noVersion)
+
+    await client.drive.writeFile(id1, 'a/hello', 'world')
+    await client.drive.writeFile(id1, 'a/goodbye', 'dog')
+    await client.drive.writeFile(id1, 'adios', 'amigo')
+    await client.drive.writeFile(id2, 'hamster', 'wheel')
+
+    t.same(await client.drive.readFile(id1, 'adios'), Buffer.from('amigo'))
+    t.same(await client.drive.readFile(id1, 'a/hello'), Buffer.from('world'))
+    t.same(await client.drive.readFile(id2, 'hello'), Buffer.from('world'))
+    t.same(await client.drive.readFile(id2, 'hamster'), Buffer.from('wheel'))
+
+    await client.drive.unmount(id1, 'a')
+    try {
+      await client.drive.readFile(id1, 'a/hello')
+    } catch (err) {
+      t.true(err)
+      t.same(err.code, 2)
+    }
+
+    await client.drive.close(id1)
+    await client.drive.close(id2)
+  } catch (err) {
+    t.fail(err)
+  }
+
+  await cleanup()
+  t.end()
+
 })
